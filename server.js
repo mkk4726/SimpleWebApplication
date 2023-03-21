9ca1c59cb54fa70fe739dafdce1b8cb0f8867a81
mkk4726 <mkk4726@naver.com>
2023-03-21 12:17:46 +0900

    Another ways using http module

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 var http = require('http');
 
-http.createServer(function (request, response) {
+// 1 
+// http.createServer(function (request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.write('Hello nodejs');
+//   response.end()
+// }).listen(8888);
+// localhost:8888로 접속 가능
+
+// 2
+// function onRequest (request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.write('Hello nodejs');
+//   response.end()
+// };
+
+// http.createServer(onRequest).listen(8888)
+
+// 3
+var server = http.createServer();
+
+// request event 처리
+server.addListener('request', function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Hello nodejs');
   response.end()
-}).listen(8888);
-// localhost:8888로 접속 가능
+});
+
+// connection event 처리
+server.addListener('connection', function(socket) {
+  console.log('connected...');
+});
+
+server.listen(8888);
