e090b6f0fddda86413575b1714d380ac76da7a0c
mkk4726 <mkk4726@naver.com>
2023-03-21 12:26:42 +0900

    Applying modularization
    Because files are getting bigger, change files to module.

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 var http = require('http');
 
-// 1 
-// http.createServer(function (request, response) {
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.write('Hello nodejs');
-//   response.end()
-// }).listen(8888);
-// localhost:8888로 접속 가능
+function start() {
+  function onRequest(request, response) {
+    console.log('request received');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  }
 
-// 2
-// function onRequest (request, response) {
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.write('Hello nodejs');
-//   response.end()
-// };
+  http.createServer(onRequest).listen(8888);
 
-// http.createServer(onRequest).listen(8888)
+  console.log('server has started');
+}
 
-// 3
-var server = http.createServer();
-
-// request event 처리
-server.addListener('request', function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello nodejs');
-  response.end()
-});
-
-// connection event 처리
-server.addListener('connection', function(socket) {
-  console.log('connected...');
-});
-
-server.listen(8888);
+exports.start = start;
